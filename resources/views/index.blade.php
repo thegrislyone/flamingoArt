0e4d4d51d4b03a6036b82c104acc7ba5ec87acab
Roman <roma.leviczkij@bk.ru>
2021-04-26 21:02:35 +0300

    hotfix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,23 +13,6 @@
 
   <!-- favicon -->
   <link rel="shortcut icon" href="assets/images/logo-icon.png" type="image/png">
-  
-  <!-- Yandex.Metrika counter -->
-  <script type="text/javascript" >
-    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-    ym(76617528, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true,
-        ecommerce:"dataLayer"
-    });
-  </script>
-  <noscript><div><img src="https://mc.yandex.ru/watch/76617528" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-  <!-- /Yandex.Metrika counter -->
 
   <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
