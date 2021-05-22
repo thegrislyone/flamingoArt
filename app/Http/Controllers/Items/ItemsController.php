90bebf48d5c94653df4141eee2153247eb59f51a
Roman <roma.leviczkij@bk.ru>
2021-05-22 23:46:44 +0300

    hotfix

--- app/Http/Controllers/Items/ItemsController.php
+++ app/Http/Controllers/Items/ItemsController.php
@@ -607,10 +607,18 @@ class ItemsController extends Controller
 
         // make items-list-version
 
+        if (!file_exists('storage/items/items_list/')) {
+            mkdir('storage/items/items_list/', 666, true);
+        }
+
         $image = Image::make(implode('/', $originalPathExploded_optimized));
         $image->save('storage/items/items_list/' . $fileName, 10);
 
         // make item-page-version
+
+        if (!file_exists('storage/items/item_page/')) {
+            mkdir('storage/items/item_page/', 666, true);
+        }
 
         $image = Image::make(implode('/', $originalPathExploded_optimized));
         // $image = $this->putWatermark($image);
